b887561afdcc2bd507605288700f08eba026e753
FadyMilad <fady@gmail.com>
2026-02-17 14:46:18 +0200

    add project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HabitIq());
+}
+
+class HabitIq extends StatelessWidget {
+  const HabitIq({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
